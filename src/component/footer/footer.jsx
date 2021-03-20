7ee8b00439cb692a91b37b63a6bbb6d263b9a4db
MarkCodering <58692435+MarkCodering@users.noreply.github.com>
2021-03-20 18:58:11 +0800

    contents upload

--- src/component/footer/footer.jsx
+++ src/component/footer/footer.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-import ButtonBase from '@material-ui/core/ButtonBase';
+import {Grid} from '@material-ui/core/Grid';
+import {Paper} from '@material-ui/core/Paper';
+import {Typography} from '@material-ui/core/Typography';
+import {ButtonBase} from '@material-ui/core/ButtonBase';
 
 import './footer_style.css';
 import company_img from './footer_1.jpg';
